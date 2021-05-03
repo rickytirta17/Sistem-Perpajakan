59543be19a3b5f93cbec43cec057a03b6be98ab9
Ricky Tirta Hardiyanta <83198877+rickytirta17@users.noreply.github.com>
2021-05-03 21:18:42 +0700

    Update Login.java

--- Login/Login.java
+++ Login/Login.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 public class Login {
     public static void main(String[] args) {
         LoginFrame frame = new LoginFrame();
-        frame.setTitle("Login Form");
+        frame.setTitle("Login Form Sistem Pajak");
         frame.setVisible(true);
         frame.setBounds(10, 10, 370, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
